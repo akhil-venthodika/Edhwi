0b4cb0f1f9bb94e1ac02aff6468b1978b74d504a
Akhil Venthodika <32707341+akhil-venthodika@users.noreply.github.com>
2022-03-02 10:01:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+    document.addEventListener("mousemove", parallax);
+    // document.addEventListener("touchstart", parallax);
+    document.addEventListener("touchmove", parallax1);
+    // document.addEventListener("touchend", parallax);
+    function parallax(e){
+      // alert("test");
+      document.querySelectorAll(".object").forEach(function(move){
+
+        var moving_value = move.getAttribute("data-value");
+        var x = (e.clientX * moving_value) / 200;
+        var y = (e.clientY * moving_value) / 200;
+
+        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+      });
+    }
+    function parallax1(e){
+      // alert("test");
+      document.querySelectorAll(".object").forEach(function(move){
+
+        var moving_value = move.getAttribute("data-value");
+        var x = (e.touches[0].clientX * moving_value) / 100;
+        var y = (e.touches[0].clientY * moving_value) / 100;
+
+        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+      });
+    }
+  
+  
